eda7d9cbf81af022ce37c9285d4f2df924eacb7d
Ndivhuwo <ndivhuwojse@gmail.com>
2026-03-22 22:57:32 +0200

    Fix server: bind to 0.0.0.0 for Railway

--- server.js
+++ server.js
@@ -15,7 +15,8 @@ const MIME_TYPES = {
 };
 
 const server = http.createServer((req, res) => {
-    let filePath = req.url === '/' ? '/index.html' : req.url;
+    const urlPath = req.url.split('?')[0];
+    let filePath = urlPath === '/' ? '/index.html' : urlPath;
     filePath = path.join(__dirname, filePath);
 
     const ext = path.extname(filePath);
@@ -32,6 +33,6 @@ const server = http.createServer((req, res) => {
     });
 });
 
-server.listen(PORT, () => {
+server.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on port ${PORT}`);
 });
